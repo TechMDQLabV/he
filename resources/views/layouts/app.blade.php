ea608795d04009af8712a7b31cddfa0006b3f645
MauroYini <mauro8792yini@gmail.com>
2020-02-08 16:39:38 -0300

    auth andando

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,109 +1,80 @@
 <!doctype html>
-<html lang="es">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" type="image/ico" href="{{ asset('./images/favicon.ico') }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'HE Hidro Extinción')</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Font Awesome Icons -->
-    <link href="{{asset('./vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Theme CSS - Includes Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <link href="{{asset('./css/main.css')}}" rel="stylesheet">
-    @yield('styles')
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-<body id="general" class="" onload="addFile()">
-<div class="hidden">
-    <script type="text/javascript">
-        <!--//--><![CDATA[//><!--
-        let usuario = "consultas";
-        let dominio = "patutoycia.com.ar";
-        let conector = "@";
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-        function dame_correo() {
-            return usuario + conector + dominio;
-        }
+                    </ul>
 
-        function escribe_enlace_correo() {
-            document.write(`<a href='mailto: ${dame_correo()}' class='t-black-link'>${dame_correo()}</a>`);
-        }
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
-        function getCorreo() {
-            let p = document.getElementById("correo");
-            let link = document.createElement("a");
-            link.setAttribute("href",`mailto:${dame_correo()}`);
-            link.classList.add("t-black-link");
-            link.innerHTML = dame_correo();
-            p.appendChild(link);
-        }
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-        var images = new Array()
-        function preload() {
-            for (i = 0; i < preload.arguments.length; i++) {
-                images[i] = new Image();
-                images[i] = preload.arguments[i];
-            }
-        }
-        preload(
-                "{{ asset('./images/backgrounds/back0.jpg') }}",
-				"{{ asset('./images/backgrounds/back1.jpg') }}",
-				"{{ asset('./images/backgrounds/back2.jpg') }}",
-				"{{ asset('./images/backgrounds/back3.jpg') }}",
-				"{{ asset('./images/backgrounds/back4.jpg') }}",
-				"{{ asset('./images/backgrounds/back5.jpg') }}",
-				"{{ asset('./images/backgrounds/back6.jpg') }}",
-				"{{ asset('./images/backgrounds/back7.jpg') }}",
-				"{{ asset('./images/backgrounds/back8.jpg') }}"
-        )
-        //--><!]]>
-    </script>
-</div>
-<div class="loader"></div>
-@include('includes.navbar')
-<div class="wrapper">
-    @yield('content')
-</div>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
 </body>
-
-<!--   Core JS Files   -->
-<!--<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-@yield('scripts')
-
-<script>
-    const getRandomRango = (min,max) => {
-        return Math.floor(Math.random()*(max-min)+min);
-    }
-
-    const getBackFile = () => {
-        return images[getRandomRango(0,images.length)];
-    }
-
-    const addFile = () => {
-        let div = document.getElementById("general");
-        div.style.backgroundImage = `url(${getBackFile()})`;
-    }
-
-    setInterval(function() {
-        addFile();
-    }, 15000);
-
-    $("window").ready(function() {
-        $(".loader").fadeOut("slow");
-    });
-</script>
-
 </html>
